a59de9ef707e4d7d8e0817e228f0de84bbbb4311
ErnestKnowing <e3050901@gmail.com>
2023-09-01 10:40:26 +0300

    feat: incomplete solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let strArray = str.split("");
+  let dictionary = {};
+  for (const [open, close] of bracketsConfig) {
+    dictionary[open] = close;
+    dictionary[close] = null;
+  }
+  for (let i = 0; i < strArray.length; i++) {
+    let char = strArray[i];
+    if (dictionary[char] === null) {
+      stack.pop() === char;
+    } else {
+      stack.push(dictionary[char]);
+    }
+  }
+  return stack.length !== 0 ? false : true;
+};
+
+  
